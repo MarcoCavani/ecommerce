d58b42f84cf7df321258bffb51459a1788ee7272
MarcoCavani <cavaniaustralia@gmail.com>
2022-01-29 12:43:25 +0800

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import data from './data';
+import Product from './component/Product';
+function App() {
+  return (
+
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <a className="brand" href="/">Shop</a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="singin">SingIn</a>
+          </div>  
+        </header>
+        <main>
+           <div className="row center">
+             {
+               data.product.map((product) =>(
+
+                <Product key={product.id}  product={product} />
+
+               ))
+             }
+               
+            
+
+           </div>
+        </main>
+        <footer className="row center">
+            All right reserved
+        </footer>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,60 @@
+const data = {
+    product:[
+        {
+            _id: '001',
+            name: 'the beast',
+            category: 'scooters',
+            image: './images/scooter-1.png',
+            price: 1200,
+            Brand: 'THE BEAST',
+            rating: 4.5,
+            numReviews:10,
+            description: 'OFF-ROAD ELECTRIC SCOOTER- THE BEAST - DUAL MOTOR 3600 WATTS PEAK POWER'
+
+
+        },
+
+        {
+            _id: '002',
+            name: 'dragon gtr v2',
+            category: 'scooters',
+            image: './images/scooter-2.png',
+            price: 1100,
+            Brand: 'DRAGON',
+            rating: 5,
+            numReviews:11,
+            description: 'ELECTRIC SCOOTER- DRAGON GTR V2 - DUAL MOTOR 1600 WATTS MAX 2400 WATTS - 2022 MODEL'
+
+
+        },
+
+        {
+            _id: '003',
+            name: 'nine boot',
+            category: 'scooters',
+            image: './images/scooter-3.png',
+            price: 1200,
+            Brand: 'nine boot',
+            rating: 4,
+            numReviews:14,
+            description: 'nine boot scooter'
+
+
+        },
+
+        {
+            _id: '004',
+            name: 'sagaway',
+            category: 'scooters',
+            image: './images/scooter-4.png',
+            price: 899,
+            Brand: 'sagaway',
+            rating: 5,
+            numReviews:12,
+            description: 'sagaway electric scooter'
+
+
+        },
+    ],
+};
+export default data;
